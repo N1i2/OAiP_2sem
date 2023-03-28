ec53c45cc30a249ed9ec6997e9542b3bc2afae97
Николай <109921087+N1i2@users.noreply.github.com>
2023-03-28 14:11:18 +0300

    Add files via upload

---
+++ laba_7/osnova_var13/fun.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+struct element//���� � ���� ���������
+{
+	int numb;
+	element* next;
+};
+
+int dob(element*&);//������� ���������� ��������
+
+int clear(element*&);//������� �������� ����� �����
+
+int save(element*&);//������� ���������� ����� � ����
+
+int read(element*&);//������� ������ ����� �� �����
+
+int schet(element*&);//������� ���������
