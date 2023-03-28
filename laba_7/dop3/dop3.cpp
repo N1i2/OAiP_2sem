32bbab700782b67a623c803de870d3a9edfaa84f
Николай <109921087+N1i2@users.noreply.github.com>
2023-03-28 14:16:06 +0300

    Add files via upload

---
+++ laba_7/dop3/dop3.cpp
@@ -0,0 +1,78 @@
+﻿#include<iostream>
+#include<Windows.h>
+#include"fun.h"//подключение заголовочноего файла
+
+using namespace std;
+//var 10
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	HANDLE col{ GetStdHandle(STD_OUTPUT_HANDLE) };
+
+	int var{};
+	element* first{ NULL };//начало стека
+
+	while (true)
+	{
+		SetConsoleTextAttribute(col, 15);
+		system("cls");
+
+		cout << "variant\n1 - add new number\n2 - clear all stek\n3 - input stek in file\n4 - output stek from file \n5 - locate numer\n6 - end\n";
+		cin >> var;
+		if (!cin)
+		{
+			cout << "error\n";
+			cin.clear();
+			cin.ignore(100, '\n');
+			SetConsoleTextAttribute(col, 0);
+			system("pause");
+			continue;
+		}
+
+		switch (var)
+		{
+		case 1:
+			dob(first) ? cout << "number added\n" : cout << "error\n";
+			SetConsoleTextAttribute(col, 0);
+			system("pause");
+			break;
+
+		case 2:
+			clear(first) ? cout << "number delete\n" : cout << "stek empty\n";
+			SetConsoleTextAttribute(col, 0);
+			system("pause");
+			break;
+
+		case 3:
+			save(first) ? cout << "stek input in file\n" : cout << "error\n";
+			SetConsoleTextAttribute(col, 0);
+			system("pause");
+			break;
+
+		case 4:
+			read(first) ? cout << "number in stek\n" : cout << "file empty\n";
+			SetConsoleTextAttribute(col, 0);
+			system("pause");
+			break;
+
+		case 5:
+			int f;
+			f = schet(first);
+			f ? cout << "there are " << f << " such numbers\n" : cout << "there are no such numbers or error\n";
+			clear(first);
+			SetConsoleTextAttribute(col, 0);
+			system("pause");
+			break;
+
+		case 6:
+			cout << "end\n";
+			SetConsoleTextAttribute(col, 0);
+			return 0;
+			break;
+
+		default:
+			cout << "error\n";
+			break;
+		}
+	}
+}
---
+++ laba_7/dop3/func.cpp
@@ -0,0 +1,153 @@
+﻿#include<iostream>
+#include<fstream>
+#include"fun.h"
+using namespace std;
+
+int dob(element*& p)
+{
+	int numb{};
+
+	cout << "enter the numbr:\t";
+	cin >> numb;
+	if (!cin)
+	{
+		cin.clear();
+		cin.ignore(100, '\n');
+		return 0;//0 - ������
+	}
+
+	element* newP = new element;//��������� ����� ��� ������
+
+	newP->numb = numb;
+	newP->next = p;
+	p = newP;
+
+	return 1;//1 - ��� �����
+}
+
+int clear(element*& p)
+{
+	if (p == NULL)//�������� ���� �� ��� �������
+		return 0;
+
+	element* del;//�������������� ��������������� ���������
+
+	while (p)
+	{
+		del = p;
+		p = p->next;
+		delete del;
+	}
+
+	return 1;
+}
+
+int save(element*& p)
+{
+	if (p == NULL)
+		return 0;
+
+	string adres{ "C:\\Users\\Admin\\Desktop\\univer\\OAiP\\7\\dop3\\info.txt" };
+
+	ofstream file{};
+	file.open(adres);
+	if (!file.is_open())
+		return 0;
+
+	element* help;//��� ��������� ��� �� ����� ���� �������� �� ��� ���� �������� 
+
+	while (p)
+	{
+		help = p;
+		p = p->next;
+		file << help->numb << '\n';
+		delete help;
+	}
+
+	file.close();
+
+	return 1;
+}
+
+int read(element*& p)
+{
+	string adres{ "C:\\Users\\Admin\\Desktop\\univer\\OAiP\\7\\dop3\\info.txt"  };
+	int numb, t{};
+
+	ifstream file;
+	file.open(adres);
+	if (!file.is_open())
+		return 0;
+
+	while (!file.eof())
+	{
+		t++;
+		file >> numb;
+		if (!cin)
+		{
+			cin.clear();
+			cin.ignore(100, '\n');
+			return 0;
+		}
+
+		element* newP = new element;
+
+		newP->numb = numb;
+		newP->next = p;
+		p = newP;
+	}
+
+	element* del;
+	//������� ��������� ���������� ��������(��������)
+	del = p;
+	p = p->next;
+	delete del;
+
+	file.close();
+
+	if (t == 1)//���� ����
+		return 0;
+
+	return 1;
+}
+
+int schet(element*& p)
+{
+	if (p == NULL)
+		return 0;
+	int first{}, second{}, t{};
+	cout << "enter the miniml number:\t";
+	cin >> first;
+	if (!cin)
+	{
+		cin.clear();
+		cin.ignore(100, '\n');
+		return 0;
+	}
+	cout << "enter the maximum number:\t";
+	cin >> second;
+	if (!cin)
+	{
+		cin.clear();
+		cin.ignore(100, '\n');
+		return 0;
+	}
+	if (first > second)
+		return 0;
+
+	element* help;
+
+	while (p)
+	{
+		help = p;
+		p = p->next;
+		if (help->numb >= first && help->numb <= second)
+		{
+			t++;
+			cout << t << " : " << help->numb << '\n';
+		}
+		delete help;
+	}
+
+	return t;
+}
