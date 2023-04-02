391bc01df403b4ee1f726a259e661932dbb362f6
Николай <109921087+N1i2@users.noreply.github.com>
2023-04-02 14:48:30 +0300

    Add files via upload

---
+++ laba_10/osn_var13/les2.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int s(int);
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int numb{};
+
+	cout << "Введите неотрецатеьное число:\t";
+	cin >> numb;
+	
+	if (!cin||numb<0)
+	{
+		cout << "Это не число\n";
+		return;
+	}
+
+	numb=s(numb)+10;
+	cout << numb;
+}
+
+int s(int n)
+{
+	if (n <= 100)
+	{
+		n += 4;
+		s(n);
+	}
+	else
+	{
+		n -= 4;
+		return n += 4;
+	}
+}
---
+++ laba_10/osn_var13/osnova.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int nn(int);
+void ntn(int, int);
+
+int ans{};
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int numb{};
+	
+	cout << "Введите число:\t";
+	cin >> numb;
+	if (!cin || numb <= 0)
+	{
+		cout << "Это число не подходт\n";
+		return;
+	}
+
+	nn(numb);
+	cout<<"Таких чисел:\t"<<ans<<'\n';
+}
+
+int nn(int n)
+{
+	int i{};
+
+	ntn(n, i);
+
+	return ans;
+}
+
+void ntn(int n, int i)
+{
+	int x{};
+	i++;
+	if (n % i == 0)
+		x++;
+	if (i != n)
+		ntn(n, i);
+
+	if (x == 1)
+		ans++;
+}
