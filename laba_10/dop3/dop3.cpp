8aaa7e1854f3e55dc82ad2b392efb2beb0e6586e
Николай <109921087+N1i2@users.noreply.github.com>
2023-04-06 13:40:13 +0300

    Add files via upload

---
+++ laba_10/dop3/dop3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#define n 5
+
+using namespace std;
+
+int NextSet(int* a)
+{
+    int j;
+    static int numb{ 1 };
+
+    do
+    {
+        j = n - 1;
+
+        while (j != -1 && a[j] >= a[j + 1])
+            j--;
+        if (j == -1)
+            return 0;
+
+        int k = n - 1;
+
+        while (a[j] >= a[k])
+            k--;
+        swap(a[j], a[k]);
+
+        int l = j + 1, r = n - 1;
+
+        while (l < r)
+            swap(a[l++], a[r--]);
+    } while (j > n - 1);
+    cout.width(3);
+    cout << ++numb << ":  ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+    NextSet(a);
+}
+
+void main()
+{
+    int a[n]{};
+
+    for (int i = 0; i < n; i++)
+        a[i] = i + 1;
+
+    cout.width(3);
+    cout << n / n << ":  ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+    NextSet(a);
+}
